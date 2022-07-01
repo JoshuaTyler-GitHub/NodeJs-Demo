b5f75a254cc9c5422764281c19994a92eb5918d3
Joshua Tyler <Joshua.4.Tyler@gmail.com>
2022-07-01 15:35:51 -0400

    May2022 Fullstack

--- websocket/index.js
+++ websocket/index.js
@@ -1,34 +1,45 @@
-// Libraries
-const http = require("http");
-const ws = require("websocket");
+// node_modules
+const http = require('http');
+const ws = require('websocket');
 
 // constants
-const port = process.env.PORT || Number("3000");
-const httpServer = http.createServer().listen(port);
+const port = process.env.PORT || Number('3001');
+const httpServer = http.createServer();
 const webSocketServer = new ws.server({ httpServer });
 const clients = [];
 
-// Client Handshake
-webSocketServer.on("request", (request) => {
-  const clientId = Number(clients.length);
+// [Client] connect
+webSocketServer.on('request', (request) => {
+  const clientId = String(request.remoteAddress || 'localhost');
   const clientConnection = request.accept(null, request.origin);
-  clients.push({
+
+  // add client to array
+  const client = {
     connection: clientConnection,
     id: clientId,
-    isConnected: true,
-  });
-  clientConnection.send(`Welcome, client: "#${clientId}".`);
+    isConnected: Boolean(true),
+  }
+  clients.push(client);
+
+  // listen for client messages
+  clientConnection.on('message', (message) => onMessage(client, message));
+
+  // welcome the client
   console.log(`Client "${clientId}" connected.`);
+  clientConnection.send(`Welcome, client: "#${clientId}".`);
 });
 
-// Client Disconnect
-webSocketServer.on("close", (clientConnection) => {
+// [Client] message
+const onMessage = async (client, message) => {
+  console.log(`Received message from: "${client.id}".`, message);
+  onResponse(client);
+};
+
+// [Client] disconnect
+webSocketServer.on('close', (clientConnection) => {
   for (const index in clients) {
-    if (
-      clients[index].connection.remoteAddress ===
-      clientConnection.remoteAddress
-    ) {
-        clients[index].connection.remoteAddress = null;
+    if (clients[index].connection.remoteAddress === clientConnection.remoteAddress) {
+      clients[index].connection.remoteAddress = null;
       clients[index].isConnected = false;
       console.log(`Client "${clients[index].id}" disconnected.`);
       break;
@@ -36,14 +47,24 @@ webSocketServer.on("close", (clientConnection) => {
   }
 });
 
-// NodeJS shutdown
-process.on("SIGINT", () => {
+// [Server] start
+httpServer.listen(port, () => {
+  console.log(`Server running at port: "${port}".`);
+});
+
+// [Server] response
+const onResponse = async (client) => {
+  client.connection.send('Thank you for your message!');
+};
+
+// [Server] shutdown
+process.on('SIGINT', () => {
   try {
-    webSocketServer.broadcast("WebSocket Server is shutting down.");
+    webSocketServer.broadcast('WebSocket Server is shutting down.');
     webSocketServer.closeAllConnections();
     webSocketServer.shutDown();
   } catch (error) {
-    console.error("Experienced error closing WebSocket:", error);
+    console.error('Experienced error closing WebSocket:', error);
   }
-  process.exit(0);
+  process.exit();
 });
